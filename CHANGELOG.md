c355c9a35ca40f3226f1cb7e0c00ae06cecc9ec8
Ubuntu <ubuntu@ip-172-31-36-165.eu-central-1.compute.internal>
2021-07-30 17:12:22 +0000

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,172 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+## [Unreleased]
+
+## [0.25.0]
+### Add
+- Add `video_html_attrs` and `audio_html_attrs` options, #272.
+
+## [0.24.1] - 2021-07-22
+### Fix
+- Export `WebRtcStreamerContext` and `WebRtcStreamerState` from the package, #342.
+- Fix `app_videochat.py`, #342.
+
+## [0.24.0] - 2021-07-16
+### Add
+- Support forking and mixing streams, #318.
+
+## [0.23.12] - 2021-07-15
+### Fix
+- Remove `MediaTrackConstraintSet.deviceId` option, which has no effect, #328.
+
+## [0.23.11] - 2021-07-13
+### Fix
+- Internal fix about event loop management, #323.
+
+## [0.23.10] - 2021-07-10
+### Fix
+- Fix type annotations on `VideoProcessTrack`, #317.
+
+## [0.23.9] - 2021-07-06
+### Fix
+- Fix CI error, #307.
+
+## [0.23.8] - 2021-07-06
+### Fix
+- Internal fix about component value management, #305.
+- Internal fix about SDP offer handling, #304.
+
+## [0.23.7] - 2021-07-05
+### Fix
+- Revert a change in v0.23.3, #301.
+
+## [0.23.6] - 2021-07-05
+### Fix
+- Fix SessionState, #299.
+
+## [0.23.5] - 2021-07-04
+### Fix
+- Make the identity of a context object consistent over the session, #298.
+
+## [0.23.4] - 2021-07-03
+### Fix
+- Internal fix about event loop management, #282.
+- Hotfix for marshalling the component value, #290.
+
+## [0.23.3] - 2021-06-30
+### Fix
+- Fix internal code on signalling, #278.
+
+## [0.23.2] - 2021-06-28
+### Fix
+- Fix internal state management, #274.
+
+## [0.23.1] - 2021-06-27
+### Fix
+- Fix to call super methods from the frontend component to adjust iframe height propery, #273.
+
+## [0.23.0] - 2021-06-27
+### Add
+- Add `desired_playing_state` argument to control the playing state programatically, #266.
+
+## [0.22.3] - 2021-06-26
+### Fix
+- Fix frontend to handle errors from promises properly, #267.
+- Fix to stop `AsyncMediaProcessTrack` when its input track stops, #269.
+  - A bug introduced in v0.22.2 (#268) has been fixed.
+
+## [0.22.2] - 2021-06-26
+### Fix
+- Fix to use `MediaRelay`, #263.
+- Fix to use the event loop attached to the Tornado app, #260.
+- Add `app_record.py` as an example of `MediaRecorder`, #264.
+
+## [0.22.1] - 2021-06-20
+### Fix
+- Fix to unset the worker when the state is not playing, #255.
+
+## [0.22.0] - 2021-06-20
+### Add
+- Make processors effective in RECVONLY mode with a player, #254.
+
+## [0.21.0] - 2021-06-17
+### Add
+- Support setting a complex `MediaStreamConstraints` object through `ClientSettings.media_stream_constraints`, #243.
+
+## [0.20.1] - 2021-05-19
+### Fix
+- Fix to unset the answer SDP after a WebRTC session closed, #206.
+- Fix `SessionState` to have a unique ID of this specific library to avoid conflicts with other SessionState instances, #210.
+
+## [0.20.0] - 2021-05-09
+### BREAKING CHANGES
+- `VideoTransformer` is deprecated. Use `VideoProcessor` instead. Related method names has also been changed. `VideoTransformer` API will be maintained for some releases, but be removed in the future.
+
+### Add
+- Experimental audio support with `audio_processor` and `audio_receiver` of `webrtc_streamer()`.
+
+## [0.12.0] - 2021-05-02
+### Added
+- Type annotations around `VideoTransformer` with generics, which enable type inference, for example, on `ctx.video_transformer`, #163.
+
+### Fixed
+- Hide unused media type from the device selector, #164.
+
+## [0.11.0] - 2021-04-20
+### Fixed
+- Only necessary media elements are displayed, for example, in case of video streaming, only a video element is shown and an audio element is hidden, #146.
+- Hide the "Select device" button when the mode is RECVONLY, #149.
+
+## [0.10.0] - 2021-04-17
+### Add
+- Theming, #140.
+
+## [0.9.0] - 2021-04-17
+### Fix
+- Fix SessionState to be bound to each session properly, #139
+
+## [0.8.1] - 2021-04-11
+### Fix
+- Internal fix, #126.
+- Set log level to `fsevents` logger, whose logs have been noisy, #129.
+
+## [0.8.0] - 2021-04-09
+### Fix
+- Fix a sample app, `app.py` to avoid an infinate loop.
+- Update to unset a worker attributes from a context object after stopping.
+- Dependency updates.
+
+## [0.7.2] - 2021-02-24
+### Fix
+- Dependency updates.
+
+## [0.7.1] - 2021-02-16
+### Fix
+- Dependency updates.
+
+## [0.7.0] - 2021-02-14
+### Fix
+- Add `VideoReceiver.get_frame()` and remove `VideoReceiver.frames_queue` attribute.
+
+## [0.6.3] - 2021-02-03
+### Fix
+- Set `aiortc` version to exactly `1.0.0`, as `1.1.0` and `1.1.1` cause an error. See https://github.com/whitphx/streamlit-webrtc/issues/37.
+
+## [0.6.2] - 2021-02-01
+### Fix
+- Fix webrtc_worker thread not to block the main thread when an error occurs inside it.
+
+## [0.6.1] - 2021-01-27
+### Fix
+- Update dependencies.
+
+## [0.6.0] - 2021-01-21
+### Added
+- Recording input/output streams.
+- Error messages in case `navigator.mediaDevices` or `getUserMedia` is not available.
+- The object detection demo is updated to display the detected labels.
+
+## [0.5.0] - 2021-01-18
+### Added
+- Error indication
---
+++ streamlit_webrtc/frontend/src/WebRtcStreamer.tsx
@@ -0,0 +1,426 @@
+import {
+  Streamlit,
+  StreamlitComponentBase,
+  withStreamlitConnection,
+  ComponentProps,
+} from "streamlit-component-lib";
+import React, { ReactNode } from "react";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Alert from "@material-ui/lab/Alert";
+import DeviceSelector from "./DeviceSelector";
+import ThemeProvider from "./ThemeProvider";
+import MediaStreamPlayer from "./MediaStreamPlayer";
+import Placeholder from "./Placeholder";
+import { compileMediaConstraints, getMediaUsage } from "./media-constraint";
+
+type WebRtcMode = "RECVONLY" | "SENDONLY" | "SENDRECV";
+const isWebRtcMode = (val: unknown): val is WebRtcMode =>
+  val === "RECVONLY" || val === "SENDONLY" || val === "SENDRECV";
+const isReceivable = (mode: WebRtcMode): boolean =>
+  mode === "SENDRECV" || mode === "RECVONLY";
+const isTransmittable = (mode: WebRtcMode): boolean =>
+  mode === "SENDRECV" || mode === "SENDONLY";
+
+const setupOffer = (
+  pc: RTCPeerConnection
+): Promise<RTCSessionDescription | null> => {
+  return pc
+    .createOffer()
+    .then((offer) => {
+      console.log("Created offer:", offer);
+      return pc.setLocalDescription(offer);
+    })
+    .then(() => {
+      console.log("Wait for ICE gethering...");
+      // Wait for ICE gathering to complete
+      return new Promise<void>((resolve) => {
+        if (pc.iceGatheringState === "complete") {
+          resolve();
+        } else {
+          const checkState = () => {
+            if (pc.iceGatheringState === "complete") {
+              pc.removeEventListener("icegatheringstatechange", checkState);
+              resolve();
+            }
+          };
+          pc.addEventListener("icegatheringstatechange", checkState);
+        }
+      });
+    })
+    .then(() => {
+      const offer = pc.localDescription;
+      return offer;
+    })
+    .catch((err) => {
+      console.error(err);
+      throw err;
+    });
+};
+
+type WebRtcState = "STOPPED" | "SIGNALLING" | "PLAYING" | "STOPPING";
+
+interface State {
+  webRtcState: WebRtcState;
+  sdpOffer: RTCSessionDescription | null;
+  signallingTimedOut: boolean;
+  videoInput: MediaDeviceInfo | null;
+  audioInput: MediaDeviceInfo | null;
+  stream: MediaStream | null;
+  error: Error | null;
+}
+
+const SIGNALLING_TIMEOUT = 10 * 1000;
+
+class WebRtcStreamer extends StreamlitComponentBase<State> {
+  private pc: RTCPeerConnection | undefined;
+  private signallingTimer: NodeJS.Timeout | undefined;
+
+  constructor(props: ComponentProps) {
+    super(props);
+
+    this.state = {
+      webRtcState: "STOPPED",
+      sdpOffer: null,
+      signallingTimedOut: false,
+      videoInput: null,
+      audioInput: null,
+      stream: null,
+      error: null,
+    };
+  }
+
+  private processAnswerInner = async (
+    pc: RTCPeerConnection,
+    sdpAnswerJson: string
+  ): Promise<void> => {
+    const sdpAnswer = JSON.parse(sdpAnswerJson);
+    console.log("Receive answer sdpOffer", sdpAnswer);
+    await pc.setRemoteDescription(sdpAnswer);
+    console.log("Remote description is set");
+  };
+
+  private processAnswer = (
+    pc: RTCPeerConnection,
+    sdpAnswerJson: string
+  ): void => {
+    this.processAnswerInner(pc, sdpAnswerJson)
+      .then(() => {
+        if (this.signallingTimer) {
+          clearTimeout(this.signallingTimer);
+        }
+        this.setState({
+          webRtcState: "PLAYING",
+          sdpOffer: null,
+        });
+      })
+      .catch((error) => {
+        this.setState({ error });
+        this.stop();
+      });
+  };
+
+  private startInner = async () => {
+    const mode = this.props.args["mode"];
+    if (!isWebRtcMode(mode)) {
+      throw new Error(`Invalid mode ${mode}`);
+    }
+
+    this.setState({
+      webRtcState: "SIGNALLING",
+      stream: null,
+      error: null,
+    });
+
+    const config: RTCConfiguration =
+      this.props.args.settings?.rtc_configuration || {};
+    console.log("RTCConfiguration:", config);
+    const pc = new RTCPeerConnection(config);
+
+    // Connect received audio / video to DOM elements
+    if (mode === "SENDRECV" || mode === "RECVONLY") {
+      pc.addEventListener("track", (evt) => {
+        const stream = evt.streams[0]; // TODO: Handle multiple streams
+        this.setState({
+          stream,
+        });
+      });
+    }
+
+    // Set up transceivers
+    if (mode === "SENDRECV" || mode === "SENDONLY") {
+      const constraints = compileMediaConstraints(
+        this.props.args.settings?.media_stream_constraints,
+        this.state.videoInput?.deviceId,
+        this.state.audioInput?.deviceId
+      );
+      console.log("MediaStreamConstraints:", constraints);
+
+      if (constraints.audio || constraints.video) {
+        if (navigator.mediaDevices == null) {
+          // Ref: https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia#privacy_and_security
+          // > A secure context is, in short, a page loaded using HTTPS or the file:/// URL scheme, or a page loaded from localhost.
+          throw new Error(
+            "navigator.mediaDevices is undefined. It seems the current document is not loaded securely."
+          );
+        }
+        if (navigator.mediaDevices.getUserMedia == null) {
+          throw new Error("getUserMedia is not implemented in this browser");
+        }
+
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        stream.getTracks().forEach((track) => {
+          pc.addTrack(track, stream);
+        });
+      }
+
+      if (mode === "SENDONLY") {
+        for (const transceiver of pc.getTransceivers()) {
+          transceiver.direction = "sendonly";
+        }
+      }
+    } else if (mode === "RECVONLY") {
+      pc.addTransceiver("video", { direction: "recvonly" });
+      pc.addTransceiver("audio", { direction: "recvonly" });
+    }
+
+    console.log("transceivers", pc.getTransceivers());
+
+    this.pc = pc;
+
+    await setupOffer(pc).then((offer) => {
+      if (offer == null) {
+        throw new Error("Failed to create an offer SDP");
+      }
+
+      this.setState({
+        sdpOffer: offer,
+      });
+    });
+  };
+
+  private start = (): void => {
+    if (this.state.webRtcState !== "STOPPED") {
+      return;
+    }
+
+    this.setState({ signallingTimedOut: false });
+    this.signallingTimer = setTimeout(() => {
+      this.setState({ signallingTimedOut: true });
+    }, SIGNALLING_TIMEOUT);
+
+    this.startInner().catch((error) =>
+      this.setState({ webRtcState: "STOPPED", sdpOffer: null, error })
+    );
+  };
+
+  private stopInner = async (): Promise<void> => {
+    if (this.state.webRtcState === "STOPPING") {
+      return;
+    }
+
+    const pc = this.pc;
+    this.pc = undefined;
+
+    this.setState({ webRtcState: "STOPPING", sdpOffer: null });
+
+    if (pc == null) {
+      return;
+    }
+
+    // close transceivers
+    if (pc.getTransceivers) {
+      pc.getTransceivers().forEach(function (transceiver) {
+        if (transceiver.stop) {
+          transceiver.stop();
+        }
+      });
+    }
+
+    // close local audio / video
+    pc.getSenders().forEach(function (sender) {
+      sender.track?.stop();
+    });
+
+    // close peer connection
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        pc.close();
+        resolve();
+      }, 500);
+    });
+  };
+
+  private stop = () => {
+    this.stopInner()
+      .catch((error) => this.setState({ error }))
+      .finally(() => {
+        this.setState({
+          webRtcState: "STOPPED",
+          sdpOffer: null,
+          stream: null,
+        });
+      });
+  };
+
+  private reconcilePlayingState = () => {
+    const desiredPlayingState = this.props.args["desired_playing_state"];
+    if (desiredPlayingState != null) {
+      if (
+        desiredPlayingState === true &&
+        this.state.webRtcState === "STOPPED"
+      ) {
+        this.start();
+      } else if (
+        desiredPlayingState === false &&
+        (this.state.webRtcState === "SIGNALLING" ||
+          this.state.webRtcState === "PLAYING")
+      ) {
+        this.stop();
+      }
+    }
+  };
+
+  private reconcileComponentValue = (prevState: State) => {
+    if (this.state === prevState) {
+      return;
+    }
+
+    const playing = this.state.webRtcState === "PLAYING";
+    const prevPlaying = prevState.webRtcState === "PLAYING";
+    const playingChanged = playing !== prevPlaying;
+
+    const sdpOffer = this.state.sdpOffer;
+    const prevSdpOffer = prevState.sdpOffer;
+    const sdpOfferChanged = sdpOffer !== prevSdpOffer;
+
+    if (playingChanged || sdpOfferChanged) {
+      if (sdpOffer) {
+        console.log("Send SDP offer", sdpOffer);
+      }
+      Streamlit.setComponentValue({
+        playing,
+        sdpOffer: sdpOffer ? sdpOffer.toJSON() : "", // `Streamlit.setComponentValue` cannot "unset" the field by passing null or undefined, so here an empty string is set instead when `sdpOffer` is undefined. // TODO: Create an issue
+      });
+    }
+  };
+
+  public componentDidMount() {
+    super.componentDidMount();
+
+    this.reconcilePlayingState();
+  }
+
+  // @ts-ignore  // TODO: Fix the base class definition
+  public componentDidUpdate(prevProps: ComponentProps, prevState: State) {
+    super.componentDidUpdate();
+
+    this.reconcilePlayingState();
+
+    this.reconcileComponentValue(prevState);
+
+    if (this.pc == null) {
+      return;
+    }
+    const pc = this.pc;
+    if (pc.remoteDescription == null) {
+      const sdpAnswerJson = this.props.args["sdp_answer_json"];
+      const prevSdpAnswerJson = prevProps.args["sdp_answer_json"];
+      const sdpAnswerJsonChanged = sdpAnswerJson !== prevSdpAnswerJson;
+      if (sdpAnswerJsonChanged) {
+        if (sdpAnswerJson && this.state.webRtcState === "SIGNALLING") {
+          this.processAnswer(pc, sdpAnswerJson);
+        }
+      }
+    }
+  }
+
+  private handleDeviceSelect = (
+    video: MediaDeviceInfo | null,
+    audio: MediaDeviceInfo | null
+  ) => {
+    this.setState({ videoInput: video, audioInput: audio });
+  };
+
+  public render = (): ReactNode => {
+    const desiredPlayingState = this.props.args["desired_playing_state"];
+    const buttonDisabled =
+      this.props.disabled ||
+      (this.state.webRtcState === "SIGNALLING" &&
+        !this.state.signallingTimedOut) || // Users can click the stop button after signalling timed out.
+      this.state.webRtcState === "STOPPING" ||
+      desiredPlayingState != null;
+    const mode = this.props.args["mode"];
+    const { videoEnabled, audioEnabled } = getMediaUsage(
+      this.props.args.settings?.media_stream_constraints
+    );
+    const receivable = isWebRtcMode(mode) && isReceivable(mode);
+    const transmittable = isWebRtcMode(mode) && isTransmittable(mode);
+
+    return (
+      <ThemeProvider theme={this.props.theme}>
+        <Box>
+          {this.state.error && (
+            <Alert severity="error">
+              {this.state.error.name}: {this.state.error.message}
+            </Alert>
+          )}
+          <Box py={1} display="flex">
+            {this.state.stream ? (
+              <MediaStreamPlayer
+                stream={this.state.stream}
+                userDefinedVideoAttrs={this.props.args.video_html_attrs}
+                userDefinedAudioAttrs={this.props.args.audio_html_attrs}
+              />
+            ) : (
+              receivable && (
+                <Placeholder
+                  loading={this.state.webRtcState === "SIGNALLING"}
+                />
+              )
+            )}
+          </Box>
+          <Box display="flex" justifyContent="space-between">
+            {this.state.webRtcState === "PLAYING" ||
+            this.state.webRtcState === "SIGNALLING" ? (
+              <Button
+                variant="contained"
+                onClick={this.stop}
+                disabled={buttonDisabled}
+              >
+                Stop
+              </Button>
+            ) : (
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={this.start}
+                disabled={buttonDisabled}
+              >
+                Start
+              </Button>
+            )}
+            {transmittable && (
+              <DeviceSelector
+                videoEnabled={videoEnabled}
+                audioEnabled={audioEnabled}
+                onSelect={this.handleDeviceSelect}
+                value={{
+                  video: this.state.videoInput,
+                  audio: this.state.audioInput,
+                }}
+              />
+            )}
+          </Box>
+        </Box>
+      </ThemeProvider>
+    );
+  };
+}
+
+// "withStreamlitConnection" is a wrapper function. It bootstraps the
+// connection between your component and the Streamlit app, and handles
+// passing arguments from Python -> Component.
+//
+// You don't need to edit withStreamlitConnection (but you're welcome to!).
+export default withStreamlitConnection(WebRtcStreamer);
